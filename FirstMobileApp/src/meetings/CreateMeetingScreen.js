969c1782e6bca6375e547efe9edecb24882cb638
DamianIanni <iannigabrieldamian@gmail.com>
2022-09-14 23:08:41 -0300

    Commit and push in case of failure implementation of Google API Key and Maps

--- FirstMobileApp/src/meetings/CreateMeetingScreen.js
+++ FirstMobileApp/src/meetings/CreateMeetingScreen.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StatusBar, Text, FlatList, Button, TouchableOpacity, Image } from "react-native";
 import users from "../../mocks/users";
-import RNPickerSelect from "react-native-picker-select";
-
+import DatePicker from 'react-native-date-picker';
 
 import { OnMeeting } from "../components/OnMeeting";
 import { HeaderForScreen } from "../components/HeaderForScreen";
 
 export const CreateMeetingScreen = (props, { navigation }) => {
+
+    const [time, setTime] = useState(new Date())
+    const [open, setOpen] = useState(false)
+
+    const [date_1, setDate_1] = useState(new Date())
+    const [open_1, setOpen_1] = useState(false)
+
+    const [selectDate, setSelectDate] = useState("Select date")
+    const [selectTime, setSelecTime] = useState("Select time")
+
+    const [selectLocation, setSelectLocation] = useState("Select location")
 
     const styles = {
         main_container: {
@@ -15,9 +25,7 @@ export const CreateMeetingScreen = (props, { navigation }) => {
             alignItems: 'center',
             justifyContent: 'flex-start',
             backgroundColor: 'gray',
-            // color: 'black',
             flex: 1,
-            // height: '100%'
         }
     }
     /// recibir usuario al momento del Login para despues saber que DATA usar ///
@@ -32,12 +40,80 @@ export const CreateMeetingScreen = (props, { navigation }) => {
             <StatusBar barStyle="dark-content" hidden={false} backgroundColor='#F4E201' translucent={false} />
             <HeaderForScreen props={props} />
             <View>
-                <TouchableOpacity>
+                <TouchableOpacity
+                    style={{ flexDirection: 'row', alignItems: 'center' }}
+                >
+                    <Image
+                        style={{ height: 105, width: 105, tintColor: '#F4E201' }}
+                        source={require("../assets/map.png")}
+                    />
+                    <Text>
+                        {selectLocation}
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={{ flexDirection: 'row', alignItems: 'center' }}
+                    onPress={() => setOpen_1(true)}
+                >
+                    <Image
+                        style={{ height: 100, width: 100, tintColor: '#F4E201' }}
+                        source={require("../assets/calendar.png")}
+                    />
+                    <Text>
+                        {console.log(date_1)}
+                        {selectDate}
+                    </Text>
+                    <DatePicker
+                        title={null}
+                        mode={"date"}
+                        textColor={'#F4E201'}
+                        modal
+                        open={open_1}
+                        date={date_1}
+                        onConfirm={(date) => {
+                            setOpen_1(false)
+                            setDate_1(date)
+                        }}
+                        onCancel={() => {
+                            setOpen_1(false)
+                        }}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={{ flexDirection: 'row', alignItems: 'center' }}
+                    onPress={() => setOpen(true)}
+                >
+                    <Image
+                        style={{ height: 100, width: 100, tintColor: '#F4E201' }}
+                        source={require("../assets/hour.png")}
+                    />
+                    <Text>
+                        {console.log(time)}
+                        {selectTime}
+                    </Text>
+                    <DatePicker
+                        title={null}
+                        is24hourSource={'locale'}
+                        mode={"time"}
+                        textColor={'#F4E201'}
+                        modal
+                        open={open}
+                        date={time}
+                        onConfirm={(time) => {
+                            setOpen(false)
+                            setTime(time)
+                        }}
+                        onCancel={() => {
+                            setOpen(false)
+                        }}
+                    />
+                </TouchableOpacity>
+                {/* <TouchableOpacity>
                     <Image
                         style={{ height: 100, width: 100, tintColor: '#F4E201' }}
                         source={require("../assets/user_profile.png")}
                     />
-                </TouchableOpacity>
+                </TouchableOpacity> */}
 
 
 
